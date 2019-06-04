a6b69a8a4dfb915a9d7f06f8855c9c7d89a98274
Jack Gay <jackpowis@gmail.com>
2019-06-04 09:25:07 -0400

    commit

--- rails_shows_api/client/src/App.js
+++ rails_shows_api/client/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      apiData: null,
+      apiDataLoaded: false,
+    };
+  }
+
+  componentDidMount = async () => {
+    const quotes = await axios.get('http://localhost:4567/quotes');
+    const apiData = quotes.data;
+  }
+  
+  
+
+  showQuotesOnPage() {
+    return this.state.apiData.map((quote) => {
+      return (
+        <div className="quote" key={quote.id}>
+          <p className="content">{quote.content}</p>
+          <span className="author">{quote.author}</span>
+          <span className="category">{quote.category}</span>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          {(this.state.apiDataLoaded) ? this.showQuotesOnPage() : <p>Loading...</p>}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
