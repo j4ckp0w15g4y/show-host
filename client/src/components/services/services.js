c4b8348fed1bd2a6a28040997466905263946136
Jack Gay <jackpowis@gmail.com>
2019-06-18 16:22:35 -0400

    commit

--- client/src/components/services/services.js
+++ client/src/components/services/services.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const URL = process.env.PORT || "/api"
 
 export const get_gigs = async () => {
-    const res = await axios.get('http://localhost:4567/api/gigs');
+    const res = await axios.get('https://showhost-nyc.herokuapp.com/api/gigs');
     return res.data 
 }
 
@@ -11,7 +11,7 @@ export const create_gig = async(gig) => {
     // gig.user_id = 1
     const res = await axios({
         method: 'post',
-        url: 'http://localhost:4567/api/gigs',
+        url: 'https://showhost-nyc.herokuapp.com/api/gigs',
         data: {gig: gig},
         // headers: {
             // Authorization: `Bearer ${token}`
@@ -21,18 +21,18 @@ export const create_gig = async(gig) => {
 }
 
 export const get_user_gigs = async (userId) => {
-    const res = await axios.get(`http://localhost:4567/api/users/${userId}/gigs`);
+    const res = await axios.get(`https://showhost-nyc.herokuapp.com/api/users/${userId}/gigs`);
     return res.data 
 }
 
 export const delete_gig = async(gigId) => {
     const res = await axios({
         method: 'delete',
-        url: `http://localhost:4567/api/gigs/${gigId}`,
+        url: `https://showhost-nyc.herokuapp.com/api/gigs/${gigId}`,
     })
 }
 
 export const update_gig = async(gigId, data) => {
-    const res = await axios.put(`http://localhost:4567/api/gigs/${gigId}`, data)
+    const res = await axios.put(`https://showhost-nyc.herokuapp.com/api/gigs/${gigId}`, data)
     return res.data 
 }
